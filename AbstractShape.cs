5b15c1b0099261f6031cd5853b44da311f77f4df
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-04-05 13:57:57 +0530

    Add project files.

---
+++ AbstractShape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_AbstractClass
+{
+    abstract public class AbstractShape
+    {
+        abstract public void CalculateArea();
+    }
+    public class Reactangle : AbstractShape
+    {
+        private int w;
+        private int l;
+        private int result;
+
+        public Reactangle(int width, int lengthg)
+        {
+            w = width;
+            l = lengthg;
+
+        }
+        public override void CalculateArea()
+        {
+            result = w * l;
+        }
+        public override string ToString()
+        {
+            return "Result is" + result;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reactangle shape = new Reactangle(54, 89);
+            shape.CalculateArea();
+            Console.WriteLine(shape);
+            Console.ReadLine();
+
+
+        }
+    }
+}
